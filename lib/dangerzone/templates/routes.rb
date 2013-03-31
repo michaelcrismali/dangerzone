5c6994167737ffb3049018959ca0bcb04159bdaa
Michael Crismali <michael.crismali@gmail.com>
2013-03-31 14:45:52 -0500

    made routes template more transparent

--- lib/dangerzone/templates/routes.rb
+++ lib/dangerzone/templates/routes.rb
@@ -1,13 +1,13 @@
 APPNAME::Application.routes.draw do
 #find this ^ and draw in the stuff below
 
-  resources :sessions, :only => [ :create, :new ]
-  get '/signin' => 'sessions#new', :as => 'sign_in'
-  delete '/sign-out' => 'sessions#destroy', :as => 'sign_out'
+  post '/sessions' => 'sessions#create', as: 'sessions'
+  get '/signin' => 'sessions#new', as: 'sign_in'
+  delete '/sign-out' => 'sessions#destroy', as: 'sign_out'
 
-  resources :create_accounts, :only => [ :new, :create ]
-  get '/signup' => 'create_accounts#new', :as => 'sign_up'
-  get '/signup/:id/:reset_password_token' => 'create_accounts#confirm', :as => 'confirm'
+  post '/create_accounts' => 'create_accounts#create', as: 'create_accounts'
+  get '/signup' => 'create_accounts#new', as: 'sign_up'
+  get '/signup/:id/:reset_password_token' => 'create_accounts#confirm', as: 'confirm'
   get '/check_your_email' => 'create_accounts#check_your_email', as: 'check_your_email'
   put '/resend_confirmation_email' => 'create_accounts#resend_confirmation_email', as: 'resend_confirmation_email'
 
