ac69a916a8cfad50cc974d4b0fc9ad07efdd87b8
Michael Crismali <michael.crismali@gmail.com>
2013-03-31 12:35:57 -0500

    stripped down sign in method

--- lib/dangerzone/templates/models/user.rb
+++ lib/dangerzone/templates/models/user.rb
@@ -3,16 +3,14 @@ class User < ActiveRecord::Base
   has_secure_password
 
   attr_accessible :email, :password, :password_confirmation, :confirmed, :remember_token
+  attr_accessible :reset_password_token, :reset_password_sent_at
 
   validates_presence_of :email
   validates_uniqueness_of :email
   validates_format_of :email, :with => /.+@.+\..+/i
 
   def sign_in(remote_ip_from_request)
-    @user.last_sign_in_ip = @user.current_sign_in_ip
-    @user.current_sign_in_ip = remote_ip_from_request #must be a parameter
-    @user.last_sign_in_at = @user.current_sign_in_at
-    @user.current_sign_in_at = Time.now
+    @user.sign_in_ip = remote_ip_from_request
     @user.sign_in_count = @user.sign_in_count + 1
     @user.save
   end
