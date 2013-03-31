28c255bb5375a72dfbf5abc936375a028d88ea40
Michael Crismali <michael.crismali@gmail.com>
2013-03-31 14:39:18 -0500

    created migration file template

---
+++ lib/dangerzone/templates/migration.rb
@@ -0,0 +1,23 @@
+class CreateUsersTable < ActiveRecord::Migration
+
+  def up
+    create_table :users do |t|
+      t.string   :email
+      t.string   :password
+      t.string   :password_confirmation
+      t.string   :password_digest
+      t.string   :sign_in_ip
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+      t.boolean  :confirmed,              :default => false
+      t.integer  :sign_in_count,          :default => 0
+
+      t.timestamps
+    end
+  end
+
+  def down
+    drop_table :users
+  end
+
+end
