6f84070e527ee6050fa942f424043447cb5bbe14
Michael Crismali <michael.crismali@gmail.com>
2013-04-01 18:20:33 -0500

    added hyphen to route names

--- lib/dangerzone/templates/routes.rb
+++ lib/dangerzone/templates/routes.rb
@@ -3,12 +3,12 @@
   get '/dangerzone' => 'create_accounts#dangerzone', as: 'dangerzone'
 
   post '/sessions' => 'sessions#create', as: 'sessions'
-  get '/signin' => 'sessions#new', as: 'sign_in'
+  get '/sign-in' => 'sessions#new', as: 'sign_in'
   delete '/sign-out' => 'sessions#destroy', as: 'sign_out'
 
   post '/create_accounts' => 'create_accounts#create', as: 'create_accounts'
-  get '/signup' => 'create_accounts#new', as: 'sign_up'
-  get '/signup/:id/:reset_password_token' => 'create_accounts#confirm', as: 'confirm'
+  get '/sign-up' => 'create_accounts#new', as: 'sign_up'
+  get '/sign-up/:id/:reset_password_token' => 'create_accounts#confirm', as: 'confirm'
   get '/check_your_email' => 'create_accounts#check_your_email', as: 'check_your_email'
   put '/resend_confirmation_email' => 'create_accounts#resend_confirmation_email', as: 'resend_confirmation_email'
 
